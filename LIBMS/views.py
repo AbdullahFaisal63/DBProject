589d388444348826e8f68e8c2c2211fdefb1fa0f
Abdullah Faisal <f219294@cfd.nu.edu.pk>
2023-04-12 17:26:19 +0500

    first

---
+++ LIBMS/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+# Create your views here.
+
+def v1(Request):
+    context = {"name":"Abdullah"}
+    return render(Request, "new.html", context)
+
+def v2(Request):
+    return HttpResponse("hello2")
