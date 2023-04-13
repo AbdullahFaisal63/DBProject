563b42bd7d3ce21483b403f4c2c81837396f14cb
Runtime terror <f219085@cfd.nu.edu.pk>
2023-04-13 17:38:15 +0500

    add push function as a practice for project

--- LIBMS/views.py
+++ LIBMS/views.py
@@ -6,7 +6,16 @@ from django.http import HttpRequest, HttpResponse
 
 def v1(Request):
     context = {"name":"Abdullah"}
-    return render(Request, "new.html", context)
+    print('hello how are you')
+    if Request.method=='POST':
+      print('hello how are you mus1')
+      fname = Request.POST.get("fname")
+      lname = Request.POST.get("lname")
+      print(fname,lname)
+      return HttpResponse('data received')
+    else:
+        print('ui')
+        return render(Request, "new.html")
 
 def v2(Request):
     return HttpResponse("hello2")
