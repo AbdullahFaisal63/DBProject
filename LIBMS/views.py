e85e4fa61b53f0f94c815efab33f9ef4e7dc8653
Runtime terror <f219085@cfd.nu.edu.pk>
2023-04-15 16:19:27 +0500

    r

--- LIBMS/views.py
+++ LIBMS/views.py
@@ -6,19 +6,12 @@ from django.http import HttpRequest, HttpResponse
 
 def v1(Request):
     context = {"name":"Abdullah"}
-    print('hello how are you')
     if Request.method=='POST':
-      print('hello how are you mus1')
-      f = "mustafa"
-      l = "rizwan"
       fname = Request.POST.get("fname")
       lname = Request.POST.get("lname")
-      if f==fname and l==lname:
-          print("your daat is available in data base")
       print(fname,lname)
       return HttpResponse('data received')
     else:
-        print('ui')
         return render(Request, "new.html")
 
 def v2(Request):
